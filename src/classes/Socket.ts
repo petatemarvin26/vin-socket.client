2891335be4f498f96bd76c3a2c8410a436268f7a
Marvin <petatemarvin26@gmail.com>
2023-01-21 19:31:56 +0800

    update

--- src/classes/Socket.ts
+++ src/classes/Socket.ts
@@ -1,3 +1,19 @@
-class Socket {}
+class Socket {
+  private _socket: WebSocket;
+  private _url: string;
+
+  constructor(url: string) {
+    this._url = url;
+    this._socket = new WebSocket(url);
+  }
+
+  getSocket() {
+    return this._socket;
+  }
+
+  getUrl() {
+    return this._url;
+  }
+}
 
 export default Socket;
